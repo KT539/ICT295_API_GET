1bbf358de7acd42e90159c9eff6fa7e87487c532
Kilian Testard <pc47xiy@eduvaud.ch>
2025-09-16 14:40:55 +0200

    linked the routes to the database

--- app.js
+++ app.js
@@ -3,54 +3,51 @@ import express from "express";
 const app = express();
 app.use(express.json());
 const port = 3000;
+import {db} from "./db/DB_connection.js"
 
 
-app.listen(port, () => { // start server on specified port
-    console.log(`Example app listening on port ${port}`); // log a message to confirm the server is running
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
 
 
 // GET method
-app.get('/', (req, res) => { // define a GET route for root URL, with a callback
-    res.json(activities); // response sent by the server to the client
+app.get('/', async (req, res) => {
+    const activities = await db.getAllActivities();
+    res.json(activities);
 });
 
-app.get("/:id", (req, res) => {
+app.get("/:id", async (req, res) => {
     const id = parseInt(req.params.id);
-    const activity = activities.find(activity => activity.id === id);
+    const activity = await db.getActivityById(id);
     res.json({activity});
 });
 
 
 // POST method
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
     console.log("dans post")
     const max_id = activities.length > 0
         ? Math.max(...activities.map(a => a.id)): 0;
-    console.log("max_id : " + max_id)
     const {name, starting_date, duration} = req.body;
     const id = max_id + 1;
-    const newActivity={id, name, starting_date, duration};
-    activities.push(newActivity);
-    const message = `The activity ${newActivity.name} was created correctly !`;
-    res.json({message : message, contact : newActivity});
+    const newActivity= await db.createActivity({id, name, starting_date, duration});
+    res.json({message : `The activity ${newActivity.name} was created correctly !`, activity : newActivity});
 });
 
 
 // PUT method
-app.put('/:id', (req, res) => {
+app.put('/:id', async (req, res) => {
     const id = parseInt(req.params.id);
     const { name, starting_date, duration } = req.body;
-    const index = activities.findIndex(activity => activity.id === id);
-    activities[index] = { id, name, starting_date, duration };
-    res.json({ message: 'Activity updated', activity: activities[index] });
+    const updatedActivity = await db.updateActivity(id, {name, starting_date, duration });
+    res.json({ message: 'Activity updated', activity: updatedActivity });
 });
 
 
 // DELETE method
-app.delete('/:id', (req, res) => {
+app.delete('/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-    const index = activities.findIndex(contact => contact.id === id);
-    activities.splice(index, 1);
-    res.json({ message: 'Activity deleted' });
+    const deletedActivity = await db.deleteActivity(id);
+    res.json({ message: 'Activity deleted', activity: deletedActivity });
 });
